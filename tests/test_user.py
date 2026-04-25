3560906611259d883f074773ec156856b660911c
Mohammad Ali Rohanian <m.rohanian@snapp.market>
2026-04-25 17:45:35 +0330

    add pytest fixtures

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,14 +1,7 @@
-from client.api_client import APIClient
-from config.config import load_config
-from services.user_service import UserService
+from tests.coftest import user_service
 
-def test_get_users():
-    config = load_config()
-
-    client = APIClient(config.base_url)
-    service = UserService(client)
-
-    user_ids, status = service.get_users()
+def test_get_users(user_service):
+    user_ids, status = user_service.get_users()
 
     assert status == 200
     assert len(user_ids) == 10
